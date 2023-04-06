1041b00510c80ac8fca9303f035c4960ec93d7b0
{alexius <{alexiussavage0@gmail.com}>
2023-04-05 23:09:44 -0700

    adding javascrpit for password variables

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,55 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var lowerCased = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCased = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
+function getRandomCharacter(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomPasswordChar = array[randomIndex];
+  return randomPasswordChar;
+}
+
+function generatePassword() 
+  var passwordLength = prompt('How many characters should the password be?');
+  var hasSpecialChars = confirm('Do you want special characters?');
+  var hasUpperCase = confirm('Do you want uppercase letters?');
+  var hasLowerCase = confirm('Do you want lowercase letters?');
+  var hasNumbers = confirm('Do you want numbers');
+​
+  var passwordOptions = {
+      passwordLength: passwordLength,
+      hasSpecialChars: hasSpecialChars,
+      hasLowerCase: hasLowerCase,
+      hasUpperCase: hasUpperCase,
+      hasNumbers: hasNumbers, 
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
