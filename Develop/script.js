4baa7db816d835c184b8df58cc7d19423781cbc6
{alexius <{alexiussavage0@gmail.com}>
2023-04-06 16:49:16 -0700

    changed var to const

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,5 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-var specialCharacters = [
+const generateBtn = document.querySelector("#generate");
+const specialCharacters = [
   '@',
   '%',
   '+',
@@ -28,24 +24,24 @@ var specialCharacters = [
   '_',
   '.',
 ];
-var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-var lowerCased = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCased = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+const lowerCased = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const upperCased = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 function getRandomCharacter(array) {
-  var randomIndex = Math.floor(Math.random() * array.length);
-  var randomPasswordChar = array[randomIndex];
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const randomPasswordChar = array[randomIndex];
   return randomPasswordChar;
 }
 
 function generatePassword() 
-  var passwordLength = prompt('How many characters should the password be?');
-  var hasSpecialChars = confirm('Do you want special characters?');
-  var hasUpperCase = confirm('Do you want uppercase letters?');
-  var hasLowerCase = confirm('Do you want lowercase letters?');
-  var hasNumbers = confirm('Do you want numbers');
+  const passwordLength = prompt('How many characters should the password be?');
+  const hasSpecialChars = confirm('Do you want special characters?');
+  const hasUpperCase = confirm('Do you want uppercase letters?');
+  const hasLowerCase = confirm('Do you want lowercase letters?');
+  const hasNumbers = confirm('Do you want numbers');
 ​
-  var passwordOptions = {
+  const passwordOptions = {
       passwordLength: passwordLength,
       hasSpecialChars: hasSpecialChars,
       hasLowerCase: hasLowerCase,
@@ -53,46 +49,42 @@ function generatePassword()
       hasNumbers: hasNumbers, 
 };
 
-var possiblePasswordChars = [];
-var password = [];
+const possiblePasswordChars = [];
+const password = [];
 
 if (passwordOptions.hasSpecialChars) {
   possiblePasswordChars=possiblePasswordChars.concat(specialCharacters);
-  var randomChar = getRandomCharacter(specialCharacters);
+  const randomChar = getRandomCharacter(specialCharacters);
   password.push(randomChar);
 }
 if (passwordOptions.hasNumbers){
   possiblePasswordChars=possiblePasswordChars.concat(hasNumbers);
-  var randomChar=getRandomCharacter(hasNumbers);
+  const randomChar=getRandomCharacter(hasNumbers);
   password.push(randomChar);
 }
 
 if (passwordOptions.hasLowerCase) {
   possiblePasswordChars=possiblePasswordChars.concat(hasLowerCase);
-  var randomChar=getRandomCharacter(hasLowerCase);
+  const randomChar=getRandomCharacter(hasLowerCase);
   password.push(randomChar);
 }
 
 if (passwordOptions.hasUpperCase) {
   possiblePasswordChars=possiblePasswordChars.concat(hasUpperCase);
-  var randomChar=getRandomCharacter(hasUpperCase);
+  const randomChar=getRandomCharacter(hasUpperCase);
   password.push(randomChar);
 }
 
-for (var i=0; i < passwordOptions.length-password.length; i++) {
-  var randomChar = getRandomCharacter(possiblePasswordChars);
+for (const i=0; i < passwordOptions.length-password.length; i++) {
+  const randomChar = getRandomCharacter(possiblePasswordChars);
   password.push(randomChar);
 }
 
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
