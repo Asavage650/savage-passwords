d74f72f71bbaa3159fc60bef516faca473dfe671
{alexius <{alexiussavage0@gmail.com}>
2023-04-10 15:27:30 -0700

    fixed bugs and fixed functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,87 @@
 const generateBtn = document.querySelector("#generate");
 const specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
-const numbers = ['0','1','2','3','4','5','6','7','8','9'];
-const lowerCased = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-const upperCased = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const lowerCased = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const upperCased = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
 
 function getRandomCharacter(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
@@ -34,52 +89,67 @@ function getRandomCharacter(array) {
   return randomPasswordChar;
 }
 
-function generatePassword() 
-  const passwordLength = prompt('How many characters should the password be?');
-  const hasSpecialChars = confirm('Do you want special characters?');
-  const hasUpperCase = confirm('Do you want uppercase letters?');
-  const hasLowerCase = confirm('Do you want lowercase letters?');
-  const hasNumbers = confirm('Do you want numbers');
-​
+function getUserOptions() {
+  const passwordLength = parseInt(
+    prompt("How many characters should the password be?")
+  );
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a valid password must be a number between 8 and 128");
+    return;
+  }
+  const hasSpecialChars = confirm("Do you want special characters?");
+  const hasUpperCase = confirm("Do you want uppercase letters?");
+  const hasLowerCase = confirm("Do you want lowercase letters?");
+  const hasNumbers = confirm("Do you want numbers");
+
   const passwordOptions = {
-      passwordLength: passwordLength,
-      hasSpecialChars: hasSpecialChars,
-      hasLowerCase: hasLowerCase,
-      hasUpperCase: hasUpperCase,
-      hasNumbers: hasNumbers, 
-};
-
-const possiblePasswordChars = [];
-const password = [];
-
-if (passwordOptions.hasSpecialChars) {
-  possiblePasswordChars=possiblePasswordChars.concat(specialCharacters);
-  const randomChar = getRandomCharacter(specialCharacters);
-  password.push(randomChar);
-}
-if (passwordOptions.hasNumbers){
-  possiblePasswordChars=possiblePasswordChars.concat(hasNumbers);
-  const randomChar=getRandomCharacter(hasNumbers);
-  password.push(randomChar);
+    passwordLength: passwordLength,
+    hasSpecialChars: hasSpecialChars,
+    hasLowerCase: hasLowerCase,
+    hasUpperCase: hasUpperCase,
+    hasNumbers: hasNumbers,
+  };
+  return passwordOptions;
 }
 
-if (passwordOptions.hasLowerCase) {
-  possiblePasswordChars=possiblePasswordChars.concat(hasLowerCase);
-  const randomChar=getRandomCharacter(hasLowerCase);
-  password.push(randomChar);
-}
+function generatePassword() {
+  const passwordOptions = getUserOptions();
 
-if (passwordOptions.hasUpperCase) {
-  possiblePasswordChars=possiblePasswordChars.concat(hasUpperCase);
-  const randomChar=getRandomCharacter(hasUpperCase);
-  password.push(randomChar);
-}
+  console.log(passwordOptions);
+  let possiblePasswordChars = [];
+  const password = [];
 
-for (const i=0; i < passwordOptions.length-password.length; i++) {
-  const randomChar = getRandomCharacter(possiblePasswordChars);
-  password.push(randomChar);
-}
+  if (passwordOptions.hasSpecialChars) {
+    possiblePasswordChars = possiblePasswordChars.concat(specialCharacters);
+    // const randomChar = getRandomCharacter(specialCharacters);
+    // password.push(randomChar);
+  }
+  if (passwordOptions.hasNumbers) {
+    possiblePasswordChars = possiblePasswordChars.concat(numbers);
+    // const randomChar=getRandomCharacter(hasNumbers);
+    // password.push(randomChar);
+  }
 
+  if (passwordOptions.hasLowerCase) {
+    possiblePasswordChars = possiblePasswordChars.concat(lowerCased);
+    // const randomChar=getRandomCharacter(hasLowerCase);
+    // password.push(randomChar);
+  }
+
+  if (passwordOptions.hasUpperCase) {
+    possiblePasswordChars = possiblePasswordChars.concat(upperCased);
+    // const randomChar=getRandomCharacter(hasUpperCase);
+    // password.push(randomChar);
+  }
+
+  for (let i = 0; i < passwordOptions.passwordLength; i++) {
+    const randomChar = getRandomCharacter(possiblePasswordChars);
+    password.push(randomChar);
+  }
+  console.log(possiblePasswordChars);
+  console.log(password);
+  return password.join("");
+}
 function writePassword() {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
@@ -88,3 +158,5 @@ function writePassword() {
 
 }
 generateBtn.addEventListener("click", writePassword);
+
+
